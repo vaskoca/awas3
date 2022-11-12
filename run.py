3afce0fa87740e9a4a79de46d2677ad54586e326
vaskoca <obreynimbo@gmail.com>
2022-11-12 10:45:54 +0000

    First Commit

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from pygooglenews import GoogleNews
 from flask import Flask
 from flask import Flask
 from flask import Response
-from flask_ngrok import run_with_ngrok #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku
+#from flask_ngrok import run_with_ngrok #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku
 import json
 import pandas as pd
 
