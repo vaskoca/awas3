17ddb8bb0723d3c6af1223490d27aad761a8a98a
vaskoca <obreynimbo@gmail.com>
2022-11-12 10:02:10 +0000

    First Commit

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 from pygooglenews import GoogleNews
+from flask import Flask
+from flask import Flask
+from flask import Response
+from flask_ngrok import run_with_ngrok #hanya digunakan ketika menggunakan google colab dan tidak untuk di deploy ke heroku
+import json
 import pandas as pd
 
 gn = GoogleNews(lang='id', country='id)')
 search = gn.search('banjir akibat hujan')
 
-for i in search['entries']:
-  print(i)
+articles = search['entries']
+for i in articles:
+  print(i.title)
